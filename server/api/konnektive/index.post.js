35afe0549dff99d68a6002064e1d1734cf9ddd12
Max Globa <onyx18121990@gmail.com>
2023-12-26 17:18:56 +0300

    a

--- server/api/konnektive/index.post.js
+++ server/api/konnektive/index.post.js
@@ -1,84 +1,33 @@
-import stripe from './auth';
+const getUrl = (object, endpoint) => {
+  let urlParams = '';
+  object.ipAddress = process.env.K_IPAD
+  object.loginId = process.env.K_USER
+  object.password = process.env.K_PASS
+  for(let item in object){
+    urlParams += `${item}=${encodeURIComponent(object[item])}&`;
+  }
+  urlParams = urlParams.slice(0, -1);
+  return `https://api.konnektive.com${endpoint}/?${urlParams}`
+}
+
+const konnektive = (url) => {
+  return $fetch(url, {method: 'POST'})
+}
 
 export default defineEventHandler(async event => {
   const body = await readBody(event)
-  let customerID, paymentID, changeID
-  return stripe.customers.create({
-    email: body.billing.email,
-    name: body.billing.firstName+' '+body.billing.lastName,
-    phone: '+1'+body.billing.phone,
-    address: {
-      city: body.billing.city,
-      country: 'US',
-      line1: body.billing.adress,
-      line2: body.billing.apartment,
-      postal_code: body.billing.zip,
-      state: body.billing.state,
-    },
-    shipping: {
-      address: {
-        city: body.shipping.city,
-        country: 'US',
-        line1: body.shipping.adress,
-        line2: body.shipping.apartment,
-        postal_code: body.shipping.zip,
-        state: body.shipping.state,
-      },
-      name: body.shipping.firstName+' '+body.shipping.lastName,
-      phone: '+1'+body.shipping.phone,
-    }
-  })
-  .then(customer => {
-    customerID = customer.id
-    return stripe.paymentMethods.create({
-      type: 'card',
-      card: {
-        number: body.payment.cardNumber,
-        exp_month: Number(body.payment.cardMonth),
-        exp_year: body.payment.cardYear,
-        cvc: body.payment.cardSecurityCode,
-      }
-    })
-  })
-  .then(payment => {
-    paymentID = payment.id
-    return stripe.paymentIntents.create({
-      customer: customerID,
-      amount: (body.amount * 100).toFixed(),
-      currency: 'usd',
-      payment_method_types: ["card"],
-      description: JSON.stringify(body.products)
-    })
-  })
-  .then(changed => {
-    return stripe.paymentIntents.confirm(changed.id, {
-      payment_method: paymentID
-    })
-  })
-  .then(result => {
-    if(result.status === 'succeeded'){
-      return {
-        status: result.status,
-        msg: 'Thank you! Your order was charged, we will contact you shortly.',
-        data: result
-      }
-    } else {
-      return {
-        status: result.status,
-        msg: 'Oops, Something went wrong please reload page and try again.',
-        data: result
-      }
-    }
-  })
-  .catch(err => {
-    console.log('catch');
-    console.dir(err);
-    return {
-      status: 'error',
-      msg: 'Error! Something went wrong please try again later.',
-      data: err
-    }
-  })
 
-
+  return konnektive(getUrl({ campaignId: 21 }, '/campaign/query'))
+    .then(response => {
+      return {
+        status: 'success',
+        msg: JSON.stringify(response.body)
+      }
+    })
+    .catch(error => {
+      return {
+        status: 'error',
+        msg: error
+      }
+    })
 })

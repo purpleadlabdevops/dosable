e1ffdd52fcfc1cb8ef25fcdb641ee67b1eb5c00a
Max Globa <onyx18121990@gmail.com>
2024-01-08 21:19:56 +0300

    Update telehealth.vue

--- pages/telehealth.vue
+++ pages/telehealth.vue
@@ -1,41 +1,6 @@
 <template>
   <section class="page page__telehealth page__info">
     <div class="container">
-      <h1>Telemedicine Consent</h1>
-      <p>Telehealth involves the utilization of electronic communication to enable healthcare providers at different locations to share individual patient medical information, with the goal of improving patient care. Services encompassed by telehealth include remote monitoring, tele-pharmacy, prescription refills, appointment scheduling, regional health information sharing, and non-clinical services, such as educational programs, administration, and public health. Dosable providers, including primary care practitioners, specialists, and subspecialists, may engage in telehealth consultations. This information may be used for diagnosis, therapy, follow-up, and/or education, incorporating patient medical records, medical images, live two-way audio and video, interactive audio, and output data from medical devices and sound and video files.</p>
-      <p>The electronic systems employed will incorporate network and software security protocols to protect patient identification and imaging data confidentiality. Security measures will be in place to safeguard data against intentional or unintentional corruption.</p>
-      <p>Primary responsibility for your medical care should remain with your local primary care doctor, and they should also maintain responsibility for your medical record.</p>
-      <p>Expected Benefits:</p>
-      <ul>
-        <li>Improved access to medical care from your local healthcare site.</li>
-        <li>More efficient medical evaluation and management.</li>
-        <li>Access to the expertise of specialists.</li>
-      </ul>
-      <p>Possible Risks:</p>
-      <ul>
-        <li>Delays in medical evaluation and treatment due to equipment or technological deficiencies.</li>
-        <li>In rare instances, the provider may reschedule a telehealth consult due to inadequate transmitted information.</li>
-        <li>Security protocol failures may pose a risk to the privacy of personal medical information.</li>
-        <li>Lack of access to complete medical records may result in adverse reactions, errors, or drug interactions.</li>
-      </ul>
-      <p>I hereby consent to receiving Dosable’s services via telehealth technologies. I understand that telehealth services offered by Dosable and its consulting providers do not replace the relationship between me and my primary care doctor. The appropriateness of a telehealth encounter is at the discretion of the Dosable provider.</p>
-      <p>I understand that Dosable is bound by federal and state laws to protect the privacy and security of health information. Telehealth may involve electronic communication of personal medical information to practitioners in other areas, including out of state.</p>
-      <p>I acknowledge the risk of technical failures during a telehealth encounter and agree to hold Dosable harmless for delays or information loss due to such failures.</p>
-      <p>I reserve the right to withhold or withdraw my consent to telehealth at any time without affecting my right to future care. I can suspend or terminate access to the service for any reason.</p>
-      <p>In the event of a medical emergency, I will dial 911, as Dosable specialists cannot connect me directly to local emergency services.</p>
-      <p>I acknowledge that alternatives to telehealth consultations, such as in-person services, are available. Parts of services involving physical tests may be conducted at my location or a testing facility.</p>
-      <p>I understand that video images and audio recordings of me may be captured and stored electronically for evaluation, training, or educational purposes.</p>
-      <p>I acknowledge that the anticipated benefits of telehealth services are not guaranteed.</p>
-      <p>My healthcare information may be shared with others for scheduling and billing purposes. I will be informed of the presence of non-medical personnel during the consultation.</p>
-      <p>I will not be prescribed any Drug Enforcement Agency controlled substances, and no guarantee of receiving a prescription is provided.</p>
-      <p>I have the right to request a copy of my medical records.</p>
-      <p>If there is a problem with the website or related services, my sole remedy is to cease using the website or terminate access to the service. Dosable will not be liable for any damages arising from the use of telehealth services.</p>
-      <p>I understand that Dosable makes no representation that materials on the website are appropriate outside of the United States.</p>
-      <h3>Patient Consent</h3>
-      <p>I have read this document, understand its terms, and hereby give my informed consent to participate in a telehealth consultation under the described conditions.</p>
-      <p>By checking the Box containing "INFORMED CONSENT FOR TELEHEALTH SERVICES," I affirm that I have read, understood, and agree to the terms of this document.</p>
-      <p>&nbsp;</p>
-      <p>&nbsp;</p>
       <h2>Asynchronous Telemedicine Informed Consent</h2>
       <p>Thank you for choosing Beluga Health, P.A. (“Beluga”)! You are viewing this form because you have elected to engage in asynchronoustelemedicine visits with a Beluga healthcare practitioner. Your practitioner is a board-certified physician licensed in your state.</p>
       <p>By clicking “I Agree,” I ACKNOWLEDGE I HAVE READ AND UNDERSTAND THE TERMS PRESENTED BELOW, AND UNDERSTAND THE RISKS AND BENEFITS OF ASYNCHRONOUS TELEMEDICINE, AND BY ACCEPTING THESE TERMS OF USE I HEREBY GIVE MY INFORMED CONSENT TO PARTICIPATE IN ASYNCHRONOUS TELEMEDICINE UNDER THESE TERMS.</p>
@@ -51,18 +16,18 @@
         <li>Delays in medical evaluation and consultation or treatment may occur due to deficiencies or failures of the equipment or the Internet, which may include poor data quality, Internet outages, or other service interruption issues. You may reschedule the visit with your healthcare practitioner should these interruptions occur.</li>
         <li>Security protocols could fail, causing a breach of privacy of personal medical information.</li>
         <li>Because Beluga does not have access to your complete medical records, if you do not disclose to your healthcare practitioner a full list of your medical history including diagnoses, treatments, medications/supplements, and allergies, adverse treatment, drug interactions or allergic reactions, or other negative outcomes may occur.</li>
-        <li>Asynchronous telemedicine services are NOT emergency services and your Personal Data (as defined in the Beluga <nuxt-link to="/privacy" target="_blank">Privacy Policy</nuxt-link>) WILL NOT BE MONITORED 24/7. If you think you are experiencing a medical emergency, CALL 911 IMMEDIATELY.</li>
+        <li>Asynchronous telemedicine services are NOT emergency services and your Personal Data (as defined in the Beluga <nuxt-link to="https://customerconsents.s3.amazonaws.com/Beluga_Health_PA_Privacy_Policy.pdf" target="_blank">Privacy Policy</nuxt-link>) WILL NOT BE MONITORED 24/7. If you think you are experiencing a medical emergency, CALL 911 IMMEDIATELY.</li>
       </ul>
       <p>THE CARE YOU RECEIVE WILL BE AT THE SOLE DISCRETION OF THE HEALTHCARE PRACTITIONER WHO IS TREATING YOU, WITH NO GUARANTEE OF DIAGNOSIS, TREATMENT, OR PRESCRIPTION. THE HEALTHCARE PRACTITIONER WILL DETERMINE WHETHER OR NOT THE CONDITION BEING DIAGNOSED AND/OR TREATED IS APPROPRIATE FOR THE CARE PROVIDED THROUGH AN ASYNCHRONOUS TELEMEDICINE ENCOUNTER VIA THE PLATFORM.</p>
       <h3>Your Rights and Acknowledgements:</h3>
       <ul>
-        <li>You have the same privacy rights via asynchronous telemedicine that you would have during an in-person visit. Dissemination of any identifiable images or information from the asynchronous telemedicine visit to researchers or other entities will not occur without your written consent. For more information about how we protect your privacy, please read the Beluga Privacy Policy <nuxt-link to="/privacy" target="_blank">here</nuxt-link>.</li>
+        <li>You have the same privacy rights via asynchronous telemedicine that you would have during an in-person visit. Dissemination of any identifiable images or information from the asynchronous telemedicine visit to researchers or other entities will not occur without your written consent. For more information about how we protect your privacy, please read the Beluga Privacy Policy <nuxt-link to="https://customerconsents.s3.amazonaws.com/Beluga_Health_PA_Privacy_Policy.pdf" target="_blank">here</nuxt-link>.</li>
         <li>Asynchronous telemedicine may involve electronic communication of your personal medical information to healthcare practitioners who may be located in other areas, including out of state.</li>
         <li>You understand that you may expect the anticipated benefits from the use of asynchronous telemedicine, but that no results can be guaranteed or assured.</li>
         <li>You understand that all information submitted to Beluga via text message and entered by your healthcare practitioner in the Beluga Platform will be part of your medical record and available to you by emailing admin@belugahealth.com. This information will have the same restrictions on dissemination without your consent.</li>
         <li>You understand that your healthcare practitioner’s initial text message to you will include his/her name and credentials, and this will be recorded in the Beluga Platform as part of your medical record.</li>
         <li>You understand you may withdraw your consent and delete your patient profile at any time by emailing admin@belugahealth.com.</li>
-        <li>You understand that your healthcare information may be shared with other individuals in accordance with the Beluga <nuxt-link to="/privacy" target="_blank">Privacy Policy</nuxt-link> and regulations or laws in state or territory in which you are located.</li>
+        <li>You understand that your healthcare information may be shared with other individuals in accordance with the Beluga <nuxt-link to="https://customerconsents.s3.amazonaws.com/Beluga_Health_PA_Privacy_Policy.pdf" target="_blank">Privacy Policy</nuxt-link> and regulations or laws in state or territory in which you are located.</li>
         <li>
           You further understand that your healthcare information may be shared in the following circumstances:
           <ul>
@@ -72,7 +37,7 @@
           </ul>
         </li>
       </ul>
-      <p><b>Call/Email/Text Messaging Consent</b>: You expressly consent to allow Beluga or its healthcare practitioners to call, email, or text you (via SMS and/or MMS) with or regarding Personal Data (as defined in the Beluga <nuxt-link to="/privacy" target="_blank">Privacy Policy</nuxt-link>), appointments, or similar matters related to your telemedicine encounters using the contact information you have provided. Any calls or texts to you may be placed using an auto-dialer or a pre-recorded or artificial voice, even if your number is on a do-not-call list. Your phone carrier’s normal rates may apply. This is consent, not a condition of purchase. You may revoke this consent at any time by emailing us at admin@belugahealth.com.</p>
+      <p><b>Call/Email/Text Messaging Consent</b>: You expressly consent to allow Beluga or its healthcare practitioners to call, email, or text you (via SMS and/or MMS) with or regarding Personal Data (as defined in the Beluga <nuxt-link to="https://customerconsents.s3.amazonaws.com/Beluga_Health_PA_Privacy_Policy.pdf" target="_blank">Privacy Policy</nuxt-link>), appointments, or similar matters related to your telemedicine encounters using the contact information you have provided. Any calls or texts to you may be placed using an auto-dialer or a pre-recorded or artificial voice, even if your number is on a do-not-call list. Your phone carrier’s normal rates may apply. This is consent, not a condition of purchase. You may revoke this consent at any time by emailing us at admin@belugahealth.com.</p>
       <p>This Asynchronous Telemedicine Informed Consent is valid during your entire treatment with Beluga. </p>
     </div>
   </section>
